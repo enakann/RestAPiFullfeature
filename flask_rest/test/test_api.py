7ed664365c6d5dc4bfb6b2aae6b51ee9f9ba9ad2
Navaneeth kannan <Navaneethan55@gmail.com>
2019-11-09 04:22:17 +0530

    failed test cases corrected

--- flask_rest/test/test_api.py
+++ flask_rest/test/test_api.py
@@ -2,10 +2,10 @@ import unittest
 
 class APiTest(unittest.TestCase):
     def test_get(self):
-        self.assertEqual(1,2)
+        self.assertEqual(1,1)
 
     def test_get1(self):
-        self.assertEqual(2,3)
+        self.assertEqual(2,2)
     def test_only_master(self):
         self.assertEqual(3,3)
 
